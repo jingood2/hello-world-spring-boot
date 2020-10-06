3c06e11e7686855d449589eb5de2836c6d17ef11
jinyoungkim <jingood2@gmail.com>
2020-10-06 13:30:35 +0900

    UPdate Log2

--- src/main/java/io/datawire/labs/hellospring/HelloController.java
+++ src/main/java/io/datawire/labs/hellospring/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
                                       TimeUnit.MILLISECONDS.toMinutes(millis),
                                       TimeUnit.MILLISECONDS.toSeconds(millis) -
                                       TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
-        return String.format("Hello11111, ATCL Spring! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
+        return String.format("Hello, ATCL Spring! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
     }
 
 }
